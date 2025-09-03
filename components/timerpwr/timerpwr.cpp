1ef2c6422ddb49e0295139f6bc7b67330f2299c8
285165 <zsb@me.com>
2025-09-03 22:52:20 +0200

    a

--- components/timerpwr/timerpwr.cpp
+++ components/timerpwr/timerpwr.cpp
@@ -49,8 +49,8 @@ void TIMERPWR::update() {
       return;
   } else {
       if (this->usb_current_ != nullptr) {
-      // usb_current_f = (65536*usb_current[2]+256*usb_current[1]+usb_current[0])/100.0;
-      usb_current_f = (int16_t*)usb_current/100.0;
+      usb_current_f = (65536*usb_current[2]+256*usb_current[1]+usb_current[0])/100.0;
+      // usb_current_f = (int16_t*)usb_current/100.0;
       ESP_LOGI(TAG, "USB current: %.2f", usb_current_f );
       ESP_LOGD(TAG, "USB current: %d %d %d %d", usb_current[3],usb_current[2],usb_current[1],usb_current[0]);
       this->usb_current_->publish_state(usb_current_f);
@@ -76,8 +76,8 @@ void TIMERPWR::update() {
     return;
   } else {
       if (this->battery_current_ != nullptr) {
-      //battery_current_f = (65536*battery_current[2]+256*battery_current[1]+battery_current[0])/100.0;
-      battery_current_f = (int16_t*)battery_current/100.0;
+      battery_current_f = (65536*battery_current[2]+256*battery_current[1]+battery_current[0])/100.0;
+      // battery_current_f = (int16_t*)battery_current/100.0;
       ESP_LOGI(TAG, "Battery current read: %.2f",battery_current_f );
       ESP_LOGD(TAG, "Battery current: %d %d %d %d", battery_current[3],battery_current[2],battery_current[1],battery_current[0]);
       this->battery_current_->publish_state(battery_current_f);  
