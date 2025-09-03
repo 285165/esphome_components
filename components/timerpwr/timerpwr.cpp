860c68bfa924e1f61383b00d81f317372691f224
285165 <zsb@me.com>
2025-09-03 22:33:54 +0200

    s

--- components/timerpwr/timerpwr.cpp
+++ components/timerpwr/timerpwr.cpp
@@ -72,7 +72,8 @@ void TIMERPWR::update() {
     return;
   } else {
       if (this->battery_current_ != nullptr) {
-      battery_current_f = (65536*battery_current[2]+256*battery_current[1]+battery_current[0])/100.0;
+      //battery_current_f = (65536*battery_current[2]+256*battery_current[1]+battery_current[0])/100.0;
+      battery_current_f = (int64_t)battery_current/100.0;
       ESP_LOGI(TAG, "Battery current read: %.2f",battery_current_f );
       ESP_LOGD(TAG, "Battery current: %d %d %d %d", battery_current[3],battery_current[2],battery_current[1],battery_current[0]);
       this->battery_current_->publish_state(battery_current_f);  
