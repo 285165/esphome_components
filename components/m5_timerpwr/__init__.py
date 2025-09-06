0a55c4cf79f83a35f25b5cfe08aac33c210123e1
285165 <zsb@me.com>
2025-09-06 23:49:00 +0200

    a

--- components/m5_timerpwr/__init__.py
+++ components/m5_timerpwr/__init__.py
@@ -6,8 +6,8 @@ from esphome.const import CONF_ID
 CODEOWNERS = ["@285165"]
 MULTI_CONF = False
 
-timerpwr_ns = cg.esphome_ns.namespace("m5_timerpwr")
-M5_8encoder = m5_timerpwr_ns.class_("M5_timerpwr", i2c.I2CDevice, cg.Component)
+m5_timerpwr_ns = cg.esphome_ns.namespace("m5_timerpwr")
+M5_timerpwr = m5_timerpwr_ns.class_("M5_timerpwr", i2c.I2CDevice, cg.Component)
 
 CONF_M5_TIMERPWR = "m5_timerpwr"
 
@@ -15,7 +15,7 @@ CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(M5_timerpwr),
     }
-).extend(i2c.i2c_device_schema(0x41))
+).extend(i2c.i2c_device_schema(0x56))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
