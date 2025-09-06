7368b1bc5f033e3cdc1d6ad8c4528399a5f55f40
285165 <zsb@me.com>
2025-09-06 20:18:07 +0200

    asdas

--- components/m5_8encoder/m5_8encoder.cpp
+++ components/m5_8encoder/m5_8encoder.cpp
@@ -20,11 +20,12 @@ static const uint8_t ADDRESS = 0xff;
 void M5_8encoder::setup() {
   ESP_LOGCONFIG(TAG, "Setting up M5_8encoder...");
   if (this->read_register(VERSION, &this->version_, 1) != i2c::ERROR_OK) {
-    this->mark_failed();
+    this->version_ = 1;
+    // this->mark_failed();
     ESP_LOGCONFIG(TAG, "...");
     // return;
   }
-  this->version_ = 1;
+
   ESP_LOGCONFIG(TAG, "Firmware version %d", this->version_);
   // uint8_t data[8] = {1,1,1,1,1,1,1,1};
   // this->write_register(RESET_BASE, data, 8);
